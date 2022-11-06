605301efcdac6aeee83a2c12993d0d9bfa5ef895
Daniel McNamara <dannymc.code@gmail.com>
2022-11-05 22:29:19 -0400

    Fixed sound bug on iphones

--- components/GoatButton.js
+++ components/GoatButton.js
@@ -1,17 +1,19 @@
 import useSound from 'use-sound'
 import goat from '../public/audio/goat_scream.mp3'
 import Button from './Button_base';
-import { Howler } from 'howler';
 
 const GoatButton = () => {
   
   // const [play] = useSound(goat);
 
   const [play] = useSound(goat, {
+
     // `interrupt` ensures that if the sound starts again before it's
     // ended, it will truncate it. Otherwise, the sound can overlap.
+    
     // interrupt: true,
     html5: true, 
+    
     onend: () => {
       console.log('Sound ended!');
     },
--- components/GruntButton.js
+++ components/GruntButton.js
@@ -3,10 +3,27 @@ import grunt from '../public/audio/grunt_yay.mp3'
 import Button from './Button_base';
 
 const GruntButton = () => {
-  const [play] = useSound(grunt);
+ 
+
+  const [play] = useSound(grunt, {
+
+    // `interrupt` ensures that if the sound starts again before it's
+    // ended, it will truncate it. Otherwise, the sound can overlap.
+    
+    // interrupt: true,
+    html5: true, 
+
+    onend: () => {
+      console.log('Sound ended!');
+    },
+  });
+
+  const handleClick = () => {
+    play();
+  };
 
   return <Button 
-    button_action= {play}
+    button_action= {handleClick}
     icon_name= "volume_up" //fonts.google.com/icons?icon.style=Rounded
     button_text= "Party Yay"
   />
