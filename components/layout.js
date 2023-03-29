2e8fb84cfbf1c31cee1a751e51ba045153d69272
Daniel McNamara <dannymc.code@gmail.com>
2023-03-29 09:52:21 -0400

    Updated more og tags

--- components/layout.js
+++ components/layout.js
@@ -6,12 +6,19 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>Meeting Moods</title>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@dannyismc" />
+        <meta property="og:site_name" content="Meeting Moods" />
         <meta property="og:title" content="Meeting Moods" />
         <meta
           name="description"
           content="Bring cheer and delight to your next meeting."
         />
+        <meta property="og:type" content="website" />
         <meta
           property="og:image"
           content="https://media.graphassets.com/ePSaabwRsGREDi7AWcd9"
