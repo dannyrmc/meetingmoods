799d201a6e33008cde49b102ac0d5ef5e32737a1
Daniel McNamara <dannymc.code@gmail.com>
2023-04-06 13:46:35 -0400

    more favicons

--- components/layout.js
+++ components/layout.js
@@ -7,9 +7,15 @@ export default function Layout({ children }) {
       <Head>
         {/* Primary Meta Tags */}
         <title>Meeting Moods</title>
-        <link rel="icon" href="/favicon.png" />
-        <link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48" href="/favicon.ico"></link>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon-apple.ico"></link>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon_32x32.png" sizes="32x32" />
+        <link rel="icon" href="/favicon_48x48.png" sizes="48x48" />
+        <link rel="icon" href="/favicon_96x96.png" sizes="96x96" />
+        <link rel="icon" href="/favicon_144x144.png" sizes="144x144" />
+        <link rel="icon" href="/favicon_180x180.png" sizes="180x180" />
+        <link rel="icon" href="/favicon_192x192.png" sizes="192x192" />
+        <link rel="icon" href="/favicon_256x256.png" sizes="256x256" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_180x180.png" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
