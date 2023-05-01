1116083e89c78c11d0d4d49077a92e2be7d2d1e1
Daniel McNamara <dannymc.code@gmail.com>
2023-04-30 21:45:44 -0400

    Added footer w/ logo

---
+++ components/footer.js
@@ -0,0 +1,26 @@
+import Logo_sm from "../public/logo-white-sm.svg";
+import Logo_md from "../public/logo-white-md.svg";
+import Link from "next/link";
+
+const github = "https://github.com/dannyrmc/meetingmoods"
+
+const Footer = () => {
+  return ( 
+
+    <footer className="w-full h-16 py-3 text-slate-50 bg-gray-950 flex justify-center items-center">
+
+      <div className="flex flex-row justify-between items-center w-full px-2 md:px-12 lg:px-24 xl:px-32">
+
+        <Logo_md className="fill-gray-50 w-[120px] h-12 bg-cover bg-no-repeat inline-block" />
+
+        <div className="flex flex-row">
+        <Link className="font-sans font-bold text-gray-50 text-sm mr-4 leading-[120%] select-none grow text-left antialiased cursor-pointer" href={github}>Made by Danny Mc →</Link>
+        </div>
+      </div>
+
+    </footer>
+
+   );
+}
+ 
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -4,37 +4,28 @@
 import styles from "../styles/Home.module.css";
 import GoatButton from "../components/button-goat-scream";
 import PartyButton from "../components/button-party-yay";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1
-        className={`font-sans font-bold	text-gray-900 text-[32px] leading-[110%] tracking-[-0.01em] text-center select-none antialiased`}
-      >
-        <span className="bg-emerald-200">Cheer</span>
-        {" & "}
-        <span className="bg-amber-200">Delight</span>
-        <br />
-        for your next meeting
-      </h1>
-
-      <div className={styles.grid}>
-        <PartyButton />
-        <GoatButton />
-      </div>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="min-h-full flex flex-auto flex-col">
+      <div className="min-h-full flex flex-col flex-1 justify-center items-center mx-4">
+        <h1
+          className={`font-sans font-bold	text-gray-950 text-[32px] leading-[110%] tracking-[-0.01em] text-center select-none antialiased`}
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
+          <span className="bg-emerald-200">Cheer</span>
+          {" & "}
+          <span className="bg-amber-200">Delight</span>
+          <br />
+          for your next meeting
+        </h1>
+
+        <div className={styles.grid}>
+          <PartyButton />
+          <GoatButton />
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 }
